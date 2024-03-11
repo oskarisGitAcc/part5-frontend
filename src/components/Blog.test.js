6624e71ef76226bfc82d778de247fe567f808a44
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-03-11 18:26:38 +0200

    5.15: Blog List Tests, step 3

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -6,16 +6,15 @@ import Blog from './Blog'
 
 describe('Blog component', () => {
   let container
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://example.com',
+    likes: 10,
+    user: { name: 'Test User', username: 'testuser' }
+  }
 
   beforeEach(() => {
-    const blog = {
-      title: 'Test Blog',
-      author: 'Test Author',
-      url: 'http://example.com',
-      likes: 10,
-      user: { name: 'Test User', username: 'testuser' }
-    }
-
     container = render(
       <Blog
         blog={blog}
@@ -53,5 +52,28 @@ describe('Blog component', () => {
 
     expect(url).toBeDefined()
     expect(likes).toBeDefined()
+  })
+
+  test('like button click calls event handler twice', async () => {
+    const mockHandler = jest.fn()
+    container = render(
+      <Blog
+        blog={blog}
+        handleLike={mockHandler}
+        handleDelete={() => {}}
+        user={{}} />
+    ).container
+
+    const user = userEvent.setup()
+    const viewButtons = screen.getAllByText('View')
+    const viewButton = viewButtons[viewButtons.length - 1]
+    await user.click(viewButton)
+
+    const likeButton = screen.getByText('Like')
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
